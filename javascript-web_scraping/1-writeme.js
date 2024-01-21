a7f26b0852a43f18940b5f2523f0c066d6bf4940
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-22 10:52:05 +1100

    Fix: Added utf-8 encoding and changed to check for error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,10 +2,8 @@
 const fs = require('fs');
 const file = process.argv[2];
 const text = process.argv[3];
-fs.writeFile(file, text, (err, data) => {
+fs.writeFile(file, text, 'utf8', (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
   }
 });

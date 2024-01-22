ef1040d05e94589c011af3e3afedb749a0f53265
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-22 12:14:27 +1100

    Feat: A script that computes the number of tasks completed by user id

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const tasks = JSON.parse(body);
+  const completed = {};
+  for (const task of tasks) {
+    if (task.completed === true) {
+      if (completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      } else {
+        completed[task.userId] = completed[task.userId] + 1;
+      }
+    }
+  }
+  console.log(completed);
+});

85155dbd2e3f8f33cc2ac951f3f8a5676a9240bf
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-22 12:02:09 +1100

    Fea: A script that prints the number of moives where the character Wedge Antilles is present

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const films = JSON.parse(body).results;
+  let count = 0;
+  for (const film of films) {
+    for (const character of film.characters) {
+        const characterId = character.split('/').filter(Boolean).pop();
+        if (characterId === '18') {
+        count = count + 1;
+      }
+    }
+  }
+  console.log(count);
+});

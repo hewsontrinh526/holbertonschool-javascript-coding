681a310a0b099d3dc79a7c91dc75ae77089480da
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-30 16:03:34 +1100

    Feat: A function countStudents that returns the number of students and the number of students in each field

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,23 @@
+function countStudents(path) {
+  const fs = require('fs');
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      throw new Error('Cannot load the database');
+    }
+
+    const lines = data.split('\n');
+    let students = lines.filter((line) => line.length > 0);
+
+    students = students.map((student) => student.split(','));
+
+    const fields = students.shift();
+    const csStudents = students.filter((student) => student[3] === 'CS');
+    const sweStudents = students.filter((student) => student[3] === 'SWE');
+
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map((student) => student[0]).join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map((student) => student[0]).join(', ')}`);
+  });
+}
+
+module.exports = countStudents;

f1dc8b955aaa65e7854cfd148b770e074d773127
hewsontrinh526 <6734@holbertonstudents.com>
2024-02-01 10:35:22 +1100

    Fix: Eslint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
 const http = require('http');
+
 const port = 1245;
 
-const app = http.createServer(function (req, res) {
-    res.write('Hello Holberton School!');
-    res.end();
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 app.listen(port);

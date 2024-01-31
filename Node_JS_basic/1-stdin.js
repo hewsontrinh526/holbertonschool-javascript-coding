a199c1e0e0d11836ef08e4aa46a44438ac874161
hewsontrinh526 <6734@holbertonstudents.com>
2024-02-01 10:10:15 +1100

    Fix: Changed to remove function calling

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,9 @@
-function whatIsYourName() {
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
-  process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name) {
-      process.stdout.write(`Your name is: ${name}`);
-      process.stdout.write('This important software is now closing\n');
-      process.exit();
-    }
-  });
-}
-
-whatIsYourName();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
+});

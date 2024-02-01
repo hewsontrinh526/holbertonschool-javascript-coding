9c76739ec712cbc2aaedf8af993bf7162c976dfb
hewsontrinh526 <6734@holbertonstudents.com>
2024-02-02 10:19:33 +1100

    Fix: Fixed input line

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,7 +7,6 @@ class StudentsController {
       res.status(200).send(`This is the list of our students\nNumber of students in CS: ${studentData.CS.length}. List: ${studentData.CS.join(', ')}\nNumber of students in SWE: ${studentData.SWE.length}. List: ${studentData.SWE.join(', ')}`);      res.end();
     } catch (error) {
       res.status(500).send(error.message);
-      res.end();
     }
   }
 
@@ -16,7 +15,7 @@ class StudentsController {
       const { major } = req.params;
       if (major !== 'CS' && major !== 'SWE') {
         res.status(500).send('Major parameter must be CS or SWE');
-        res.end();
+        return res.end();
       }
       const studentData = await readDatabase(process.argv[2]);
       res.status(200).send(

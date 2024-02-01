c78b00878a0fe9dd8774a3e4f02e07163f58161a
hewsontrinh526 <6734@holbertonstudents.com>
2024-02-01 12:05:06 +1100

    Fix: Revert changes

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,37 +1,38 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    const studentData = fs.readFile(path, 'utf-8');
-    const lineData = studentData.split('\n');
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      throw new Error('Cannot load the database');
+    }
 
+    const lines = data.split('\n').slice(1);
     const csStudents = [];
     const sweStudents = [];
 
-    for (const line of lineData) {
+    for (const line of lines) {
       const student = line.split(',');
 
       if (student[3] === 'CS') {
         csStudents.push(student[0]);
-      } else if (student[3] === 'SWE') {
+      }
+      if (student[3] === 'SWE') {
         sweStudents.push(student[0]);
       }
     }
 
-    console.log(`Number of students: ${lineData.length}`);
+    console.log(`Number of students: ${lines.length}`);
     console.log(
       `Number of students in CS: ${csStudents.length}. List: ${csStudents.join(
-        ', '
-      )}`
+        ', ',
+      )}`,
     );
     console.log(
       `Number of students in SWE: ${
         sweStudents.length
-      }. List: ${sweStudents.join(', ')}`
+      }. List: ${sweStudents.join(', ')}`,
     );
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+  });
 }
 
 module.exports = countStudents;
